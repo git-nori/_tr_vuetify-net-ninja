ffac85070e267235a89149c119e367a6e1e0cbd7
HironoriSato <maza.hs5360@gmail.com>
2019-09-23 21:36:28 +0900

    [#4 - Text & Colors]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>This is a home page</h1>
+    <p
+      class="red white--text"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ipsam cupiditate in et libero blanditiis nam fuga beatae eum impedit officia temporibus voluptate error at aliquam, nobis architecto sunt atque?</p>
+    <p
+      class="pink lighten-4 red--text text--darken-4"
+    >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam fugit rerum, magni beatae sapiente dolores consectetur ea laboriosam totam, nulla nemo atque veritatis quibusdam esse aliquid quaerat laborum id nihil!</p>
+    <p class="display-4">display-4</p>
+    <p class="display-1">display-1</p>
+    <p class="headline font-weight-bold">This is headline</p>
+    <p class="subheading">This is subheading</p>
+    <p class="caption">this is a caption</p>
   </div>
 </template>
 
