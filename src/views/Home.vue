55f654dd464624e13e4652572664d1753287875b
HironoriSato <maza.hs5360@gmail.com>
2019-09-23 22:04:49 +0900

    [#6 - Breakpoints & Visibility]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,12 @@
 <template>
   <div class="home">
     <h1>This is a home page</h1>
-
-    <!-- 2つとも効果は同じ -->
-    <v-btn class="pink white--text">click me</v-btn>
-    <v-btn color="pink" dark>click me</v-btn>
-
-    <!-- 背景色は変化するが、影は持ちません。 -->
-    <v-btn color="pink" depressed>click me</v-btn>
-    <!-- text => 影と背景色を持たない、ホバーした時のみに、ボタンの形状が表示 -->
-    <v-btn text color="pink">click me</v-btn>
-
-    <v-btn class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>
-    <v-btn small class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>
-    <v-btn fab class="purple white--text">
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
+    <!-- mdサイズ以下の場合は非表示 -->
+    <v-btn class="hidden-md-and-down">hidden-md-and-down</v-btn>
+    <!-- mdサイズ以上の場合は非表示 -->
+    <v-btn class="hidden-md-and-up pink">hidden-md-and-up</v-btn>
+    <!-- smサイズの場合のみ非表示 -->
+    <v-btn class="hidden-sm-only">hidden-sm-only</v-btn>
   </div>
 </template>
 
