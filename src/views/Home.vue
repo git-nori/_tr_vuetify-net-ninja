7f19ad8ea958925a8aa8c24949f51fb9e5426777
HironoriSato <maza.hs5360@gmail.com>
2019-09-23 21:25:21 +0900

    [mod]delete and edit initial source

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,9 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>This is a home page</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
-export default {
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
