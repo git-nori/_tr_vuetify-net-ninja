45a8bd0d21f4432f19527cad553e0d0c8ce87e18
HironoriSato <maza.hs5360@gmail.com>
2019-09-24 22:20:35 +0900

    [#14 - Grid System (part 2)] vuetify2.x

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,31 @@
 <template>
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
+
+    <!-- y方向に20pxのmarginを適用 -->
+    <!-- fluid => containerの幅を横いっぱいに広げる -->
+    <v-container class="my-5" fluid>
+      <v-card flat>
+        <v-row>
+          <v-col sm="12" md="6">
+            <div class="caption grey--text">ProjectTitle</div>
+            <div>ProjectTitle</div>
+          </v-col>
+          <v-col sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>The Nori</div>
+          </v-col>
+          <v-col sm="4" md="2">
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jun 2019</div>
+          </v-col>
+          <v-col sm="4" md="2">
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,42 +1,6 @@
 <template>
   <div class="team">
     <h1 class="subheading grey--text">Team</h1>
-
-    <!-- y方向に20pxのmarginを適用 -->
-    <!-- fluid => containerの幅を横いっぱいに広げる -->
-    <v-container class="my-5" fluid>
-      <p>https://github.com/iamshaunjp/vuetify-playlist/blob/lesson-12/todo-ninja/src/views/Team.vuehttps://github.com/iamshaunjp/vuetify-playlist/blob/lesson-12/todo-ninja/src/views/Team.vue</p>
-
-      <!-- 2.0はこっち
-      <v-row justify="center">
-        <v-col md="12" sm="12">
-          <v-btn block>12</v-btn>
-        </v-col>
-        <v-col md="12" sm="6">
-          <v-btn block>6</v-btn>
-        </v-col>
-        <v-col md="12" sm="6">
-          <v-btn block>6</v-btn>
-        </v-col>
-      </v-row>
-      -->
-
-      <!-- wrap => 12を超えたら改行する -->
-      <v-layout row wrap justify-center>
-        <v-flex md12 sm12>
-          <v-btn block>12</v-btn>
-        </v-flex>
-        <v-flex md6 sm6>
-          <v-btn block>6</v-btn>
-        </v-flex>
-        <v-flex md6 sm6>
-          <v-btn block>6</v-btn>
-        </v-flex>
-        <v-flex md6 sm6>
-          <v-btn block>6</v-btn>
-        </v-flex>
-      </v-layout>
-    </v-container>
   </div>
 </template>
 
