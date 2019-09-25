671f0ff2e744e21e8c37b37c758b4dcea2818b4a
HironoriSato <maza.hs5360@gmail.com>
2019-09-25 11:25:35 +0900

    [#21 - Expansion Panels]

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,91 @@
 <template>
   <div class="projects">
     <h1 class="subheading grey--text">Projects</h1>
+
+    <v-container class="my-5">
+      <!-- [選択時のオプション]accodion => panel間を繋げる, focusable => 選択中のpanelのbgをgreyにする -->
+      <v-expansion-panels accordion focusable>
+        <v-expansion-panel v-for="(project, index) in myProjects" :key="index">
+          <v-expansion-panel-header>{{ project.title }}</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-card flat>
+              <v-card-text class="grey--text">
+                <div class="font-weight-bold">{{ project.due }}</div>
+                <div>{{ project.content }}</div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Test Expansion Panel1",
+          person: "Yoshi",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Test Expansion Panel2",
+          person: "Yoshi",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Design a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        },
+        {
+          title: "Create a community forum",
+          person: "Goken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+        }
+      ]
+    };
+  },
+  computed: {
+    // projectsの中でproject.person === 'Yoshi'のデータのみを絞り込み返す
+    myProjects() {
+      return this.projects.filter(project => {
+        return project.person === "Yoshi";
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
