d2603293b8cefe268820d34f69c37362926590b7
HironoriSato <maza.hs5360@gmail.com>
2019-09-25 14:06:51 +0900

    [#25 - Datepickers] + date-fnsライブラリで日付をformat

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -11,6 +11,12 @@
         <v-form class="px-3">
           <v-text-field label="title" prepend-icon="mdi-folder" v-model="title"></v-text-field>
           <v-textarea label="infomation" prepend-icon="mdi-pencil" v-model="content"></v-textarea>
+          <v-menu>
+            <template v-slot:activator="{on}">
+              <v-text-field label="Due date" prepend-icon="mdi-calendar" :value="dueDate" v-on="on"></v-text-field>
+            </template>
+            <v-date-picker v-model="dueDate"></v-date-picker>
+          </v-menu>
           <v-btn text small class="mt-3 success" @click="submit">ADD PROJECT</v-btn>
         </v-form>
       </v-card-text>
@@ -19,16 +25,21 @@
 </template>
 
 <script>
+// 日付に対しての処理を簡単にするライブラリ => date-fnsをインストール
+// npm install date-fns --save
+import format from "date-fns/format";
+
 export default {
   data() {
     return {
       title: "",
-      content: ""
+      content: "",
+      dueDate: ""
     };
   },
   methods: {
     submit() {
-      console.log(this.title, this.content);
+      console.log(this.title, this.content, this.dueDate);
     }
   }
 };
